921ea2023ca918e95012805513b07f0f6362c4e7
Azad Sadigli <azad.sadiqli@gmail.com>
2022-10-25 12:24:24 +0400

    log viewer added

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['log_threshold'] = 0;
 | application/logs/ directory. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = '';
+$config['log_path'] = 'application/logs/'.date("Y").'/'.date("m").'/';
 
 /*
 |--------------------------------------------------------------------------
